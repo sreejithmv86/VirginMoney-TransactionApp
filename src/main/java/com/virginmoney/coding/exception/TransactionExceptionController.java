7350dc01bf332d5330d193783d1b012d7675018d
sreejithmv86 <sreejithmvarier@gmail.com>
2022-11-02 00:51:20 +0530

    rename exception controller

--- src/main/java/com/virginmoney/coding/exception/TransactionExceptionController.java
+++ src/main/java/com/virginmoney/coding/exception/TransactionExceptionController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
-public class CategoryExceptionController {
+public class TransactionExceptionController {
 
     @ExceptionHandler(value = CategoryNotFoundException.class)
     public ResponseEntity<Object> exception(CategoryNotFoundException exception) {
